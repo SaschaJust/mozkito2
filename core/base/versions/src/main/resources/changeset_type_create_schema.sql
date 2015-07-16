84ed24ea8a709ace30b87f73c5967f46334dbb54
Sascha Just <sascha.just@own-hero.net>
2015-07-16 18:57:37 +0100

    some old residues

--- core/base/versions/src/main/resources/changeset_type_create_schema.sql
+++ core/base/versions/src/main/resources/changeset_type_create_schema.sql
@@ -2,10 +2,3 @@ CREATE TABLE changeset_types (
 	changeset_id BIGINT NOT NULL,
 	integration_type SMALLINT NOT NULL
 );
-
-CREATE TABLE integration_types (
-	value SMALLINT PRIMARY KEY,
-	name VARCHAR(32) NOT NULL
-);
-
-
